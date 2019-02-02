43dfd4d71cec4d31eca8d5be37086f70bf13e690
jackchiu7 <jc2001@scarletmail.rutgers.edu>
2019-02-02 15:09:17 -0500

    added extracting file names

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,19 +6,29 @@ Created on Sat Feb  2 11:03:38 2019
 """
 
 import tensorflow as tf
-
-
+import cv2 as cv
+import os
 
 
 
 
 def main():
-    print('a')
-    print('1')
+    trainDirs = [x[0] for x in os.walk("Training")]
+    trainLabels = os.listdir('Training')
+    trainDirs = trainDirs[1:]
+    testDirs = [x[0] for x in os.walk("Test")]
+    testDirs = testDirs[1:]
+    testLabels = os.listdir('Test')
+#    for direc in trainDirs:
+#        print(direc)
+        
+    training = {}
+    testing = {}
+    for d in trainDirs:
+        training[d] = os.listdir(d)
+    for d in testDirs:
+        testing[d] = os.listdir(d)
     
-    
-
-
 
 
 
