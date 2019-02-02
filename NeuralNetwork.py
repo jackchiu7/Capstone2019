6f8bc8908bdb63f97df414cfe50f5571ab5acc64
jackchiu7 <jc2001@scarletmail.rutgers.edu>
2019-02-02 12:48:25 -0500

    initial

---
+++ NeuralNetwork.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  2 11:03:38 2019
+
+@author: jackc
+"""
+
+import tensorflow as tf
+
+
+
+
+
+
+def main():
+    print('a')
+    print('1')
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__': 
+    main()
+
+
+
