4178163a6c535a29b86c3bf4bf70d92de525ddae
jackchiu7 <jc2001@scarletmail.rutgers.edu>
2019-02-02 19:22:15 -0500

    setup and display images`

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,13 +6,12 @@ Created on Sat Feb  2 11:03:38 2019
 """
 
 import tensorflow as tf
-import cv2 as cv
+import cv2 
 import os
+import numpy as np
+import glob
 
-
-
-
-def main():
+def openImages(limit = -1, train = True, test = True):
     trainDirs = [x[0] for x in os.walk("Training")]
     trainLabels = os.listdir('Training')
     trainDirs = trainDirs[1:]
@@ -22,15 +21,44 @@ def main():
 #    for direc in trainDirs:
 #        print(direc)
         
-    training = {}
-    testing = {}
-    for d in trainDirs:
-        training[d] = os.listdir(d)
-    for d in testDirs:
-        testing[d] = os.listdir(d)
+    training = []
+    testing = []
+#    for d in trainDirs:
+#        training.append(os.listdir(d))
+#    for d in testDirs:
+#        testing.append(os.listdir(d))
+
+    if train:
+        for i in trainLabels:
+            temp = []
+            for file in glob.glob('Training/'+ i + '/*.jpg'):
+                temp.append(cv2.imread(file))
+                if len(temp) == limit:
+                    break
+            training.append(temp)
     
+    if test:
+        for i in testLabels:
+            temp = []
+            for file in glob.glob('Test/'+ i + '/*.jpg'):
+                temp.append(cv2.imread(file))
+                if len(temp) == limit:
+                    break
+            testing.append(temp)
+    
+    return trainLabels, training, testLabels, testing
 
 
+def main():
+    
+    trainLabels, training, testLabels, testing = openImages(5, train = True, test = False)
+
+    img = training[1][2]
+    cv2.namedWindow(trainLabels[1], cv2.WINDOW_NORMAL)
+    cv2.imshow(trainLabels[1],img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
 
 
 
